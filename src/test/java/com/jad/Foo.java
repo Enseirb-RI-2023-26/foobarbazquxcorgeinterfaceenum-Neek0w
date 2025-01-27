3291181986a93d35ac13dd988192a8ee79f9fc04
neek0w <nicolaseg2003@gmail.com>
2025-01-27 15:15:46 +0100

    Getters and Setters

--- src/test/java/com/jad/Foo.java
+++ src/test/java/com/jad/Foo.java
@@ -8,12 +8,43 @@ public class Foo implements IFoo {
     private IQux qux;
     private ICorge corge;
 
-
     public Foo(IBaz baz) {
         this.baz = baz;
         this.bars = new ArrayList<>();
         this.qux = qux;
 
+    }
+
+    public IBaz getBaz() {
+        return this.baz;
+    }
+
+    public void setBaz(IBaz baz) {
+        this.baz = baz;
+    }
+
+    public ArrayList<IBar> getBars() {
+        return this.bars;
+    }
+
+    public void setBars(ArrayList<IBar> bars) {
+        this.bars = bars;
+    }
+
+    public IQux getQux() {
+        return this.qux;
+    }
+
+    public void setQux(IQux qux) {
+        this.qux = qux;
+    }
+
+    public ICorge getCorge() {
+        return this.corge;
+    }
+
+    public void setCorge(ICorge corge) {
+        this.corge = corge;
     }
 
     public void addBar(IBar bar) {
